b070686a14cf6188629a0d9576d25b1e31b4648e
Andromeda-NGC224 <shadim115115@gmail.com>
2024-12-08 17:00:27 +0200

    fixed Joi

--- src/validationJoi/diagramJoiValid.js
+++ src/validationJoi/diagramJoiValid.js
@@ -1,10 +1,15 @@
 import Joi from 'joi'
 
 export const diagramAddJoiValid = Joi.object({
-  diagramName: Joi.string().min(3).max(20).required().messages({
+  diagramName: Joi.string().min(3).required().messages({
     'string.base': 'Name must be a string',
     'string.min': 'Name must have at least 3 characters',
-    'string.max': 'Name must have no more than 20 characters',
     'any.required': 'The Name field is required',
+  }),
+  blocks: Joi.array().default([]).messages({
+    'array.base': 'Blocks must be an array',
+  }),
+  connections: Joi.array().default([]).messages({
+    'array.base': 'Connections must be an array',
   }),
 })
